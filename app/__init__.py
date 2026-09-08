954efa88a753e52f1b54b0247dd84241b710eefd
praveena230607 <240081601039@crescent.education>
2026-09-08 09:58:52 +0530

    Initialize Flask app and register YouTube blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,18 @@
+import os , urllib.parse , urllib.request render_template
+from app.youtube import youtube_bp
 
+Gemini_api_key ="Gemini_API_key";
+
+def home():
+  return render_template ("index.html")
+
+def create_app():
+
+  app = flask(_name_)
+  app.register_blueprint(youtube_bp, url_prefix="/youtube")
+
+@app.route("/html")
+def html():
+  return render_template("index.html")
+
+return app;
